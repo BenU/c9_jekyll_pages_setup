c5115224a6f91195dfc6d9df6589bc9884f32011
Benjamin Unger <benjamin.d.unger@gmail.com>
2024-09-29 08:33:27 -0400

    Create setup.sh
    Add initial commands for script.

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Prompt for user input
+read -p "Enter your name: " name
+read -p "Enter your email: " email
+read -p "Enter the repository name: " repo_name
+
+# Create the repository directory within the environment directory and initialize Git
+mkdir ~/environment/"$repo_name"
+cd ~/environment/"$repo_name"
+git init
+
+# Create a .gitignore file in the repository directory
+cat <<EOL > .gitignore
+# add appropriate files here
+
+EOL
+
+# Set up Git
+git config --global user.name "$name"
+git config --global user.email "$email"
+
+# Add useful Git aliases
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.unstage 'reset HEAD --'
+
+# Configure Vim
+cat <<EOL > ~/.vimrc
+set number
+set tabstop=2
+set shiftwidth=2
+set expandtab
+syntax on
+set autoindent
+set smartindent
+set smartcase
+set clipboard=unnamedplus
+set ruler
+set cursorline
+set wildmenu
+EOL
+
+echo "Setup complete! Your Cloud9 environment is ready for GitHub Pages development."
