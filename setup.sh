c46fdbe4cae446d43538b264b1694a4bf521fa5d
Benjamin Unger <benjamin.d.unger@gmail.com>
2024-09-29 15:51:52 -0400

    Add placeholder comment for remote GitHub repository

--- setup.sh
+++ setup.sh
@@ -28,6 +28,8 @@ git config --global alias.br branch
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'
 
+# Add remote GitHub repository
+
 # Configure Vim
 cat <<EOL > ~/.vimrc
 set number
